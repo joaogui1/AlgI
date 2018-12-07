7354988bbabf4a14d37340989e5f434a2d9aed65
lusmoura <luisamoura@usp.br>
2018-12-07 13:49:34 -0200

    Renaming BST

--- Projeto2/ABB/BST.h
+++ Projeto2/ABB/BST.h
@@ -1,5 +1,5 @@
-#ifndef TREE_H
-#define TREE_H
+#ifndef BST_H
+#define BST_H
 
 typedef int elem;
 typedef struct _bst BST;
@@ -7,32 +7,32 @@ typedef struct _bst BST;
 /*				Cria estrutura 			       */
 /* Parâmetros: - 							   */
 /* Retorno: ponteiro para a estrutura	   	   */
-BST *createABB();
+BST *createBST();
 
 /*    			Insere elemento			       */
 /*Parâmetros: BST* - ponteiro para a struct    */
 /*			  elem - valor a ser inserido      */
 /*Retorno: int - 1, se houver erro, 0 se não   */
-int insertABB(BST *t, elem x);
+int insertBST(BST *t, elem x);
 
 /*				Remove elemento 		         */
 /*Parâmetros: BST* - ponteiro para a struct      */
 /*			  elem - valor a ser removido        */
 /*Retorno: int - 1, se houver erro, 0 se não     */
-int removeABB(BST *t, elem x);
+int removeBST(BST *t, elem x);
 
 /*				Busca elemento 		   	   	     */
 /*Parâmetros: BST* - ponteiro para a struct      */
 /*			  elem - valor a ser removido        */
 /*Retorno: int - 0 se não for encontrado, 1 se for */
-int searchABB(BST *t, elem x);
+int searchBST(BST *t, elem x);
 
 /* 		   Printa arvore (para debug)            */
 /*Parâmetros: BST* - ponteiro para a struct      */
-void printAbb(BST* t);
+void printBST(BST* t);
 
 /*	    		Libera memória alocada		     */
 /*Parâmetros: BST* - ponteiro para a struct      */
-void destroyABB(BST *t);
+void destroyBST(BST *t);
 
 #endif
--- Projeto2/ABB/main.c
+++ Projeto2/ABB/main.c
@@ -1,34 +1,27 @@
-#include "ABB.h"
+#include "BST.h"
 #include <stdio.h>
 #include <time.h>
 
-// void in_order(Node *n){
-//   if(n == NULL) return;
-//   in_order(n -> left);
-//   printf("%d ", n -> info);
-//   in_order(n -> right);
-// }
-
 int main(){
-  ABB *t;
+  BST *t;
   int n = 100000, aux;
-  t = createABB();
+  t = createBST();
   clock_t c;
   
   c = clock();
   for(int i = 0; i < n; ++i){
-    insertABB(t, i);
+    insertBST(t, i);
   }
   c = clock() - c;
 
-  // printAbb(t); 
+  // printBST(t); 
   printf("%lf\n", c/(float)CLOCKS_PER_SEC);
   // for(int i = 0; i < q; ++i){
   //   scanf("%d", &aux);
-  //   RemoveABB(t, aux);
+  //   RemoveBST(t, aux);
   //   in_order(t -> root);
   //   printf("\n");
   // }
 
-  destroyABB(t);
+  destroyBST(t);
 }
