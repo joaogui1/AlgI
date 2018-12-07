c5fbeba59e8861c5e2565d292ca08c8cd3ae642b
lusmoura <luisamoura@usp.br>
2018-12-07 12:23:52 -0200

    Comments ListFreq

--- Projeto2/ListFreq/listFreq.c
+++ Projeto2/ListFreq/listFreq.c
@@ -16,8 +16,7 @@ struct _listFreq{
 listFreq* createListFreq(){
 	listFreq* newListFreq = malloc(sizeof(listFreq));
 
-	if(newListFreq == NULL)
-		return NULL;
+	if(newListFreq == NULL)	return NULL;
 
 	newListFreq->start = NULL;
 	newListFreq->end = NULL;
@@ -35,12 +34,12 @@ int insertListFreq(listFreq* lf, elem x){
 	newNode->next = NULL;
 	newNode->prev = NULL;
 
-	if(isEmptyListFreq(lf)) {
+	if(isEmptyListFreq(lf)) { //insere primeiro elemento
 		lf->start = newNode;
 		lf->end = newNode;
 		lf->end->next = lf->start;
 		lf->start->prev = lf->end;
-	} else {
+	} else { //insere no final
 		lf->end->next = newNode;
 		newNode->prev = lf->end;
 		lf->end = newNode;
@@ -52,56 +51,54 @@ int insertListFreq(listFreq* lf, elem x){
 	return 0;
 }
 
-int removeListFreq(listFreq* lf, elem x){
-	if(isEmptyListFreq(lf))
-		return 1;
-
+node* internalSearch(listFreq* lf, elem x){
 	node* target = lf->start;
 
-	for(int i = 0; i < lf->size; i++){
-		// printf("%p %d\n", target, target->info);
-		if(target->info == x){
-			//removing from start
-			if(target == lf->start){
-				lf->start = target->next;
-				lf->end->next = lf->start;
-				lf->start->prev = lf->end;
-			} else if(target == lf->end){ //removing from end
-				lf->end = target->prev;
-				lf->end->prev = target->prev->prev;
-				lf->end->next = lf->start;
-			} else{ //removing from middle
-				target->next->prev = target->prev;
-				target->prev->next = target->next;
-			}
-			lf->size--;
-			free(target);
-			return 0;
-		}
-		target = target->next;
-	}
-
-	return 1;
-}
-
-int searchListFreq(listFreq* lf, elem x){
-	node* target = lf->start;
-
+	//busca sequencial, mudando valores quando encontrados
 	for (int i = 0; i < lf->size; ++i){
 		if(target->info == x){
-			//swap values
+			//swap dos valores, caso não seja o primeiro
 			if(target != lf->start){
 				target->info ^= target->prev->info;
 				target->prev->info ^= target->info;
 				target->info ^= target->prev->info;
 			}
-			return 1;
+			return target;
 		}
 
 		target = target->next;
 	}
 
+	return NULL;
+}
+
+int removeListFreq(listFreq* lf, elem x){
+	if(isEmptyListFreq(lf))	return 1;
+
+	//busca elemento para remover
+	node* target = internalSearch(lf, x);
+	if(target == NULL) return 1;
+
+	if(target == lf->start){ //removendo do inicio
+		lf->start = target->next;
+		lf->end->next = lf->start;
+		lf->start->prev = lf->end;
+	} else if(target == lf->end){ //removendo do final
+		lf->end = target->prev;
+		lf->end->prev = target->prev->prev;
+		lf->end->next = lf->start;
+	} else{ //removendo do meio
+		target->next->prev = target->prev;
+		target->prev->next = target->next;
+	}
+	
+	lf->size--;
+	free(target);
 	return 0;
+}
+
+int searchListFreq(listFreq* lf, elem x){
+	return (internalSearch(lf, x) != NULL); 
 }
 
 int isEmptyListFreq(listFreq* lf){
@@ -112,7 +109,7 @@ void printListFreq(listFreq* lf){
 	node* curr = lf->start;
 
 	for(int i = 0; i < lf->size; i++){
-		printf("%d [%p] [%p] [%p]\n", curr->info, curr, curr->prev, curr->next);
+		printf("%d\n", curr->info);
 		curr = curr->next;
 	}
 
@@ -120,6 +117,7 @@ void printListFreq(listFreq* lf){
 }
 
 void destroyListFreq(listFreq* lf){
+	//remove todos os elementos
 	while(!isEmptyListFreq(lf)){
 		int aux = lf->start->info;
 		removeListFreq(lf, aux);
--- Projeto2/ListFreq/listFreq.h
+++ Projeto2/ListFreq/listFreq.h
@@ -4,11 +4,40 @@
 typedef int elem;
 typedef struct _listFreq listFreq;
 
+/*				Cria estrutura 			         */
+/* Parâmetros: - 							     */
+/* Retorno: ponteiro para a estrutura	   	     */
 listFreq* createListFreq();
+
+/*    			Insere elemento			         */
+/*Parâmetros: listFreq* - ponteiro para a struct */
+/*			  elem - valor a ser inserido        */
+/*Retorno: int - 1, se houver erro, 0 se não     */
 int insertListFreq(listFreq* l, elem x);
+
+/*				Remove elemento 		         */
+/*Parâmetros: listFreq* - ponteiro para a struct */
+/*			  elem - valor a ser removido        */
+/*Retorno: int - 1, se houver erro, 0 se não     */
 int removeListFreq(listFreq* l, elem x);
+
+/*				Busca elemento 		   	   	     */
+/*Parâmetros: listFreq* - ponteiro para a struct */
+/*			  elem - valor a ser removido        */
+/*Retorno: int - 0 se não for encontrado, 1 se for */
 int searchListFreq(listFreq* l, elem x);
+
+/*			   Lista está vazio				     */
+/*Parâmetros: listFreq* - ponteiro para a struct */
+/*Retorno: int - 1, se estiver vazio, 0 se não   */
 int isEmptyListFreq(listFreq* l);
+
+/*			   Printa lista (para debug)         */
+/*Parâmetros: listFreq* - ponteiro para a struct */
 void printListFreq(listFreq* l);
+
+/*	    		Libera memória alocada		     */
+/*Parâmetros: listFreq* - ponteiro para a struct */
 void destroyListFreq(listFreq* l);
+
 #endif
